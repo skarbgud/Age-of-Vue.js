7818b35a6eed93bd3db1c2589199bb8a066a87b0
Nam <sjarbgud@gmail.com>
2020-09-30 21:43:14 +0900

    질문 상세 페이지 스타일링 및 v-html 디렉티브 사용

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,15 +1,36 @@
 <template>
   <diV>
-      <!-- {{ this.$store.state.item }} -->
-      <p>{{ this.$store.state.item.title }}</p>
-      <div>
-        {{ this.$store.state.item.content }}
-      </div>
+      <section>
+        <!-- 질문 상세 장보 -->
+        <div class="user-container">
+          <div>
+            <i class="fas fa-user"></i>
+          </div>
+          <div class="user-description">
+            <router-link :to="`/user/${fetchedItem.user}`">
+              {{ fetchedItem.user }}
+            </router-link>
+            <div class="time">
+              {{ fetchedItem.time_ago }}
+            </div>
+          </div>
+        </div>
+        <h2>{{ fetchedItem.title }}</h2>
+      </section>
+      <section>
+        <!-- 질문 댓글 -->
+        <div v-html="fetchedItem.content"></div>
+      </section>      
   </diV>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
+    computed: {
+      ...mapGetters(['fetchedItem']),
+    },
     created() {
       const itemId = this.$route.params.id;
       this.$store.dispatch('FETCH_ITEM', itemId);
@@ -17,6 +38,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
 </style>
