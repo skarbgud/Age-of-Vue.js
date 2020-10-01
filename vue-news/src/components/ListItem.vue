0a3be3986233773e75697f75023acb913c92313e
Nam <sjarbgud@gmail.com>
2020-10-02 01:52:39 +0900

    template 속성과 v-if 디렉티브 사용

--- vue-news/src/components/ListItem.vue
+++ vue-news/src/components/ListItem.vue
@@ -8,14 +8,27 @@
         </div>
         <!-- 기타 정보 영역 -->
         <div>
+          <!-- 타이틀 영역 -->
           <p class="news-title">
-            <a v-bind:href="item.url">
-              {{ item.title }}
-            </a>
+            <template v-if="item.domain">
+              <a v-bind:href="item.url">
+                {{ item.title }}
+              </a>
+            </template>
+            <template v-else>
+              <router-link :to="`item/${item.id}`">
+                {{ item.title }}
+              </router-link>
+            </template>
           </p>
           <small class="link-text">
             {{ item.time_ago }} by 
-            <router-link v-bind:to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+            <router-link 
+              v-if="item.user"
+              v-bind:to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+            <a :href="item.url" v-else>
+              {{ item.domain }}
+            </a>  
           </small>
         </div>
       </li>
