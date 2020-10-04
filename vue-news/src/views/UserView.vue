73cbddedad19e55d96e419b79895a06bcdb440be
Nam <sjarbgud@gmail.com>
2020-10-04 16:46:23 +0900

    사용자 프로필 컴포넌트 소개 및 등록

--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
+    <user-profile></user-profile>
+    <!-- <p>name : {{ userInfo.id }}</p>
     <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <p>created : {{ userInfo.created }}</p> -->
   </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue';
+
 export default {
+    components: {
+      UserProfile,
+    },
     computed: {
       userInfo() {
         return this.$store.state.user;
