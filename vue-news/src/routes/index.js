069135da2b7200487f44b8c91ea493c6bfac1da5
Nam <sjarbgud@gmail.com>
2020-10-08 23:56:11 +0900

    라우터 네비게이션 가드

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -6,6 +6,8 @@ import JobsView from '../views/JobsView.vue';
 import UserView from '../views/UserView.vue';
 import ItemView from '../views/ItemView.vue';
 // import createListView from '../views/CreateListView.js';
+import bus from '../utils/bus.js';
+import { store } from '../store/index.js';
 
 Vue.use(VueRouter);
 
@@ -23,18 +25,57 @@ export const router = new VueRouter({
         // component: url 주소로 갔을 때 표시될 컴포넌트
         component: NewsView,
         // component: createListView('NewsView'),
+        beforeEnter: (to, from, next) => {
+          bus.$emit('start:spinner');  
+          store.dispatch('FETCH_LIST', to.name)
+            .then(() => {
+              console.log('fetched');
+              bus.$emit('end:spinner');
+              next();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          // console.log('to', to);
+          // console.log('from', from);
+          // console.log(next);
+        }
     },
     {
         path: '/ask',
         name: 'ask',
         component: AskView,
         // component: createListView('AskView'),
+        beforeEnter: (to, from, next) => {
+          bus.$emit('start:spinner');
+          store.dispatch('FETCH_LIST', to.name)
+            .then(() => {
+              console.log('fetched');
+              bus.$emit('end:spinner');
+              next();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
     },
     {
         path: '/jobs',
         name: 'jobs',
         component: JobsView,
         // component: createListView('JobsView'),
+        beforeEnter: (to, from, next) => {
+          bus.$emit('start:spinner');
+          store.dispatch('FETCH_LIST', to.name)
+            .then(() => {
+              console.log('fetched');
+              bus.$emit('end:spinner');
+              next();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
     },
     {
         path: '/user/:id',
