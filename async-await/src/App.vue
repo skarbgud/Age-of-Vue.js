4385465ff32e5475990eaf9d36c13b3b5c3185f3
Nam <sjarbgud@gmail.com>
2020-10-10 19:35:35 +0900

    async & await 예제

---
+++ async-await/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <button @click="loginUser">login</button>
+    <h1>List</h1>
+    <ul>
+      <li v-for="item in items" v-bind:key="item.id">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items: [],
+    }
+  },
+  methods: {
+    loginUser() {
+      axios.get('https://jsonplaceholder.typicode.com/users/1')
+        .then(response => {
+          if (response.data.id === 1) {
+            console.log('사용자가 인증되었습니다.');
+            axios.get('https://jsonplaceholder.typicode.com/todos')
+              .then(response => {
+                this.items = response.data;
+              })
+          }
+        })
+        .catch(error => console.log(error));
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
